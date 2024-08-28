06d6bad058b49f39e9ff18daaf029a2ba7756b02
Betül Kuyucu <96betul@gmail.com>
2024-08-28 21:14:45 +0300

    menu

--- src/components/carousel/MenuCarousel.jsx
+++ src/components/carousel/MenuCarousel.jsx
@@ -4,25 +4,27 @@ import { Container, Image } from 'react-bootstrap';
 const MenuCarousel = () => {
 
     const styles = {
-        backgroundColor: 'black',
-        height: '40vh', 
+        backgroundColor: 'black', // İç arka plan rengi
+        height: '40vh',
         display: 'flex',
-        justifyContent: 'space-evenly', // Yazıyı sola, resmi sağa hizalar
-        alignItems: 'end',            // İçeriği dikeyde ortalar
-        color: 'white',             
-        padding: '0 20px',               // Hem sağdan hem soldan boşluk bırakabiliriz
-        textShadow: '2px 2px 4px rgba(0,0,0,0.7)' 
+        justifyContent: 'space-evenly',
+        alignItems: 'end',
+        borderRadius: '50px', // Köşeleri yuvarlak yapar
+        border: '5px solid transparent', // Şeffaf bir sınır
+        backgroundImage: 'linear-gradient(black, black), linear-gradient(to right, #1AA6B8, #4a848b, #3CB141, #01BF01, #D2FF54)', // Kenarlar için degrade sınır
+        backgroundOrigin: 'border-box',
+        backgroundClip: 'content-box, border-box',
     };
 
     return (
         <Container style={styles}>
             <div className='pt-5'>
                 <Image src="/images/wubba2.png" width="400px">
-                    
-               </Image>
+
+                </Image>
             </div>
-            <Image src="/images/rick2.png" width="200px"
-                />
+            <Image src="/images/rick2.png" width="200px" style={{ marginLeft: '100px' }}
+            />
         </Container>
     );
 };
